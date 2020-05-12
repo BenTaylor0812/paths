8d008ac58d0ab822534a42c7bc19f4b65bcfe6ac
Ben Taylor <53377240+BenTaylor0812@users.noreply.github.com>
2020-05-12 21:58:28 +0100

    First working code
    Needs to be cleaned a little, but does (somewhat) work.

--- paths.go
+++ paths.go
@@ -1,14 +1,9 @@
 package paths
 
-import (
-	"fmt"
-)
-
 // Node describes a node structure for the shortest path algorithm.
 type Node struct {
 	Label        string
 	NodeList     map[string]int
-	distance     int
 	previousNode string
 	nextNode     string
 }
@@ -62,119 +57,51 @@ func findNode(node string, nodes []*Node) *Node {
 // ShortestPath takes in a graph (array of nodes) and returns an array of the shortest path
 func ShortestPath(graph []*Node) {
 	var remainingNodes []*Node
-	//customCopy(remainingNodes, graph)
-	var cementedNode []*Node
+	var cementedNodes []*Node
 
 	startNode := graph[0]
-	startNode.distance = 0
 	startNode.previousNode = "first"
 
 	endNode := graph[len(graph)-1]
 	endNode.nextNode = "end"
 
 	currentNode := startNode
-	// cementedNode = append(cementedNode, startNode)
-	//remainingNodes = deleteElement(remainingNodes, *startNode)
 
 	shortestDistances := map[string]int{startNode.Label: 0}
 
-	for len(cementedNode) != len(graph) {
+	for len(cementedNodes) != len(graph) {
 		var minDist = shortestDistances[currentNode.Label]
 		firstIt := true
 		for k, v := range currentNode.NodeList {
 			node := findNode(k, graph)
-			if !find(k, remainingNodes) && !find(k, cementedNode) {
+			if !find(k, remainingNodes) && !find(k, cementedNodes) {
 				remainingNodes = append(remainingNodes, node)
 				shortestDistances[node.Label] = minDist + v
-			} else if find(k, remainingNodes) && !find(k, cementedNode) {
+			} else if find(k, remainingNodes) && !find(k, cementedNodes) {
 				if minDist+v < shortestDistances[node.Label] {
 					shortestDistances[node.Label] = minDist + v
 					node.previousNode = currentNode.Label
 					currentNode.nextNode = node.Label
 				}
 			}
-
-			if v < minDist || firstIt {
-				minDist = v
-				currentNode.nextNode = k
-				firstIt = false
-			}
 		}
 
-		cementedNode = append(cementedNode, currentNode)
+		cementedNodes = append(cementedNodes, currentNode)
 		remainingNodes = deleteElement(remainingNodes, *currentNode)
 
 		var smallest int
 		var smallestNode string
 		firstIt = true
 		for k, v := range shortestDistances {
-			if v < smallest || firstIt {
+			if (v < smallest || firstIt) && find(k, remainingNodes) {
 				smallest = v
 				smallestNode = k
+				firstIt = false
 			}
 		}
-		fmt.Println(smallestNode)
+
 		currentNode = findNode(smallestNode, remainingNodes)
-
-		fmt.Printf("Cemented nodes: ")
-		for _, i := range cementedNode {
-			fmt.Printf(" [%v], ", *i)
-		}
-		fmt.Printf("\n")
-		fmt.Printf("Remaining nodes ")
-		for _, i := range remainingNodes {
-			fmt.Printf(" [%v], ", *i)
-		}
-		fmt.Printf("\n")
-		fmt.Println(shortestDistances)
-
-		// for k := range shortestDistances {
-		// 	if find(k, remainingNodes) {
-
-		// 	}
-		// }
 	}
-	// fmt.Println(shortestDistances)
-	// for _, i := range cementedNode {
-	// 	fmt.Println(*i)
-	// }
-
-	// var minDist int
-	// firstIt := true
-
-	// // fmt.Printf("Closest node is: %s, with a distance of: %d.", currentNode.nextNode, minDist)
-	// minDist = currentNode.distance + minDist
-	// currentNode = obtainNode(graph, currentNode.nextNode)
-	// cementedNode = append(cementedNode, currentNode)
-	// currentNode.distance = minDist
-	// shortestDistances[currentNode.Label] = currentNode.distance
-	// fmt.Println(shortestDistances)
-
-	// for currentNode.nextNode != "end" {
-	// 	fmt.Println(currentNode)
-	// 	firstIt = true
-	// 	for k, v := range currentNode.NodeList {
-	// 		if v < minDist || firstIt {
-	// 			minDist = v
-	// 			currentNode.nextNode = k
-	// 			firstIt = false
-	// 		}
-	// 	}
-	// 	// fmt.Printf("Closest node is: %s, with a distance of: %d.", currentNode.nextNode, minDist)
-	// 	minDist = currentNode.distance + minDist
-	// 	fmt.Println(currentNode)
-	// 	currentNode = obtainNode(graph, currentNode.nextNode)
-	// 	fmt.Println(currentNode)
-	// 	fmt.Println(endNode)
-	// 	cementedNode = append(cementedNode, currentNode)
-	// 	currentNode.distance = minDist
-	// 	shortestDistances[currentNode.Label] = currentNode.distance
-	// 	if currentNode.Label == "F" {
-	// 		break
-	// 	}
-	// }
-
-	// fmt.Println(shortestDistances)
 }
 
 // TestMain -
